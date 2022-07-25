74592a60ae63cebb8ab96bf19276832c74ac26d0
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-07-25 18:29:19 +0500

    Able to Signup and SignIn

--- src/Components/LoginPage/Login.js
+++ src/Components/LoginPage/Login.js
@@ -1,27 +1,46 @@
-import './Login.css'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../Firebase'
+import { useNavigate } from 'react-router-dom'
+import './Login.css'
 
-import React from 'react'
 
 const Login = () => {
+  let [LoEmail, setLoEmail] = useState('')
+  let [LoPass, setLoPass] = useState('')
+  let [Warns, setWarns] = useState(0);
+
+  let Navigator = useNavigate();
+  let HandleSignIn = (Event) => {
+    signInWithEmailAndPassword(auth, LoEmail, LoPass)
+      .then(() => {
+        Navigator('/')
+      })
+      .catch((Error) => {
+        setWarns(1);
+      })
+  }
   return (
     <div className='Login'>
-        <div className="Form">
-            <form action="">
-                <label htmlFor="Email">Enter Your Email</label>
-                <input id='Email' type="text" />
-                <label htmlFor="Pass">Enter Your Password</label>
-                <input id='Pass' type="password" />
+      <div className="Form">
+        <form action="" onSubmit={(E) => { HandleSignIn(); E.preventDefault() }}>
+          <label htmlFor="Email">Enter Your Email</label>
+          <input id='Email' type="email" value={LoEmail} onChange={(E) => { setLoEmail(E.target.value); setWarns(0) }} />
+          <label htmlFor="Pass">Enter Your Password</label>
+          <input id='Pass' type="password" value={LoPass} onChange={(E) => { setLoPass(E.target.value); setWarns(0) }} />
+          {Warns === 1 ? <span className='text-danger'>May Be Password Short or Email Wrong</span>:<span></span>}
 
-                <button className='Btn LoginBtn'>Login</button>
+          <button className='Btn LoginBtn'>Login</button>
 
-                <p>Don;t Have An Account , Then <Link to="/signup" style={{color:"orange"}}>SignUp</Link></p>
-            </form>
+          <p>Don't Have An Account , Then <Link to="/signup" style={{color:"orange"}}>Signup</Link></p>
 
-            <Link to="/">
-              <button className='Btn w-100'>Back to Home-Page</button>
-            </Link>
-        </div>
+        </form>
+
+        <Link to="/">
+          <button className='Btn w-100'>Back to Home-Page</button>
+        </Link>
+      </div>
     </div>
   )
 }
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,17 +1,39 @@
 import './Signup.css'
 import { Link } from 'react-router-dom'
 
-import React from 'react'
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {useNavigate} from 'react-router-dom'
+import {auth} from '../Firebase'
+import React, { useState } from 'react'
 
 const Signup = () => {
+  let [SiEmail , setSiEmail]=useState('')
+  let [SiPass , setSiPass]=useState('')
+  let [Warn,setWarn]=useState(0);
+
+  let Navigator=useNavigate();
+  let HandleSignUp=(Event)=>
+  {
+    createUserWithEmailAndPassword(auth , SiEmail , SiPass)
+    .then(()=>
+    {
+      Navigator('/')
+    })
+    .catch((Error)=>
+    {
+      setWarn(1);
+    })
+  }
   return (
     <div className='Signup'>
         <div className="Form">
-            <form action="">
+            <form action="" onSubmit={(E)=>{HandleSignUp(); E.preventDefault()}}>
                 <label htmlFor="Email">Enter Your Email</label>
-                <input id='Email' type="text" />
+                <input required={true} id='Email' type="email" value={SiEmail} onChange={(E)=>{setSiEmail(E.target.value) ;setWarn(0)}}/>
                 <label htmlFor="Pass">Enter Your Password</label>
-                <input id='Pass' type="password" />
+                <input required={true} id='Pass' type="password" value={SiPass} onChange={(E)=>{setSiPass(E.target.value);setWarn(0)}}/>
+
+                {Warn===1?<span className='text-danger'>May Be Password Short or Email Wrong</span>:<span></span> }
 
                 <button className='Btn SignupBtn'>Signup</button>
 
