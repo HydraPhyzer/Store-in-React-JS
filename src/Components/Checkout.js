9e063f9a5da6881416dbae9b2ffcd3e458b562b2
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-07-25 09:48:45 +0500

    Calculating the Total Bill

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -22,8 +22,6 @@ const Checkout = () => {
   return (
     <div className='See'>
       <Navbar />
-
-
       {
         Basket.length !== 0 ?
           Basket.map((Elem, Ind) => {
@@ -33,15 +31,8 @@ const Checkout = () => {
                   <img src={Elem.image} alt="" />
                 </div>
                 <div className="Block">
-                  <span>Price : {Elem.price}</span>
-                  <span>{StarCount(Elem.rating.rate , Elem.rating.count)}</span>
-                  <div className="Inc-Dec">
-                    <div className="btn-group btn-group-sm" role="group" aria-label="Small button group">
-                      <button type="button" className="btn btn-outline-dark">-</button>
-                      <button type="button" className="btn btn-outline-dark">{0}</button>
-                      <button type="button" className="btn btn-outline-dark">+</button>
-                    </div>
-                  </div>
+                  <span>Price : {Elem.price}$</span>
+                  <span>{StarCount(Elem.rating.rate, Elem.rating.count)}</span>
                 </div>
                 {window.matchMedia("(max-width: 450px)").matches ? <button onClick={() => { RemoveItem(Elem.id) }} className='Btn'>Remove</button> : <button onClick={() => { RemoveItem(Elem.id) }} className='Btn'>Remove From Cart</button>}
               </div>
@@ -51,6 +42,16 @@ const Checkout = () => {
           <div className="alert alert-info mt-1 g-0 text-white" style={{ backgroundColor: "#f39c12", borderRadius: "0px", color: "black" }} role="alert">
             Empty Cart <FaSadTear style={{ color: "#2c3e50", fontSize: "x-large" }} /> . Please Add Items in Me!
           </div>
+
+      }
+      {
+        Basket.length !== 0 ? <div className="alert alert-primary Total" role={"alert"}>
+        Total Bill :
+        {
+          Basket.reduce((Acc, Curr) => Acc += Curr.price, 0)
+        }
+        $
+      </div> : ""
       }
     </div>
   )
